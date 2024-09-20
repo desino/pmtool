10b7cff6a35bd7a2372c0e14eb11fecad45f0f33
Desino <india@desino.be>
2024-09-20 17:27:05 +0530

    MISC: layout & style changes in solution design, create ticket popup, sidebar initiative links

--- resources/js/components/Layout/SidebarComponent.vue
+++ resources/js/components/Layout/SidebarComponent.vue
@@ -70,21 +70,21 @@
                 </div>
                 <hr>
                 <ul class="list-group border">
-                    <li class="list-group-item fw-bold border-bottom">{{ $t('header.menu.initiative_links') }}</li>
-
-                    <a v-if="initiativeData?.share_point_url" :href="initiativeData?.share_point_url" target="_blank"
-                        class="list-group-item border-0">
-                        <i class="bi bi-folder-symlink"></i>
-                        {{ $t('header.menu.initiative.share_point_url') }}</a>
-                    <div v-if="initiativeData?.initiative_environments"
+                    <li class="list-group-item bg-desino text-white border-desino fw-bold border-bottom">{{ $t('header.menu.initiative_links') }}</li>
+                    <li v-if="initiativeData?.share_point_url" class="list-group-item border-0">
+                        <a v-if="initiativeData?.share_point_url" :href="initiativeData?.share_point_url" target="_blank" class="btn-link" style="text-decoration: none;">
+                            <i class="bi bi-folder-symlink"></i>
+                            {{ $t('header.menu.initiative.share_point_url') }}
+                        </a>
+                    </li>
+                    <li class="list-group-item border-0" v-if="initiativeData?.initiative_environments"
                         v-for="environment in initiativeData?.initiative_environments">
-                        <a :href="environment.url" target="_blank" class="list-group-item border-0">
+                        <a :href="environment.url" target="_blank" style="text-decoration: none;" class="btn-link">
                             <i class="bi bi-hdd-rack-fill"></i>
                             {{ environment.name }}
                         </a>
-                    </div>
-                    <div v-if="initiativeData?.initiative_environments?.length === 0" class="list-group-item border-0">
-                    </div>
+                    </li>
+                    <li v-if="initiativeData?.initiative_environments?.length === 0" class="list-group-item border-0"></li>
                 </ul>
             </div>
         </div>
