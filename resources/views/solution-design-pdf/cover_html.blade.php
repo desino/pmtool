5697e3ed4f8a4f1126c70cb49e09cb686ae22b11
unknown <devendra@desino.be>
2024-09-24 17:40:32 +0530

    2.3.6: Solution Design - Generate & download PDF

--- resources/views/solution-design-pdf/cover_html.blade.php
+++ resources/views/solution-design-pdf/cover_html.blade.php
@@ -1,9 +1,15 @@
 <p style="font-size: 32px;color: #FFFFFF;font-family: Nunito-Bold;">
-    {{ __('messages.solution_design_pdf_cover_page_title')}}
+    {{ __('messages.solution_design_pdf_cover_page_title') }}
 </p>
 <p style="font-size: 15px;font-family: Nunito-Bold;color: #FFFFFF;">
-    {{ __('messages.solution_design_pdf_cover_page_client_title', ['CLIENT_NAME' => $initiative->client->name])}}
+    {{ __('messages.solution_design_pdf_cover_page_client_title', ['CLIENT_NAME' => $initiative->client->name]) }}
 </p>
-<p style="font-size: 17px;font-family: Nunito-BoldItalic;color: #FFFFFF;line-height: 0.5em;">
-    {{ __('messages.solution_design_pdf_cover_page_initiative_title', ['INITIATIVE_NAME' => $initiative->name])}}
+<p style="font-size: 15px;font-family: Nunito-Bold;color: #FFFFFF;">
+    {{ __('messages.solution_design_pdf_cover_page_initiative_title', ['INITIATIVE_NAME' => $initiative->name]) }}
 </p>
+<p style="font-size: 15px;font-family: Nunito-Bold;color: #FFFFFF;">
+    {{ __('messages.solution_design_pdf_cover_page_export_at', ['DATE' => \Carbon\Carbon::now()->format('M d, Y')]) }}
+</p>
+{{-- <p style="font-size: 17px;font-family: Nunito-BoldItalic;color: #FFFFFF;line-height: 0.5em;">
+    {{ __('messages.solution_design_pdf_cover_page_initiative_title', ['INITIATIVE_NAME' => $initiative->name]) }}
+</p> --}}
