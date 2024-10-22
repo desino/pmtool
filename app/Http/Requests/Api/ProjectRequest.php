21d885ae64e7967fe1f0ff36b290d1d4d00f677d
unknown <devendra@desino.be>
2024-10-22 10:45:46 +0530

    102024-20-FI-Project name does not having Range validity message

--- app/Http/Requests/Api/ProjectRequest.php
+++ app/Http/Requests/Api/ProjectRequest.php
@@ -23,7 +23,7 @@ class ProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string',
+            'name' => 'required|string|max:255',
         ];
     }
 }
