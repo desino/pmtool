91d7d084b9f859db8414c7e330c0b459ea87ef16
unknown <devendra@desino.be>
2024-11-13 15:04:17 +0530

    DEV202411-5:Search on initiative selection

--- resources/js/Page/Initiative/HeaderInitiativeDropBoxComponent.vue
+++ resources/js/Page/Initiative/HeaderInitiativeDropBoxComponent.vue
@@ -1,10 +1,20 @@
 <template>
-    <select class="form-select form-select-sm" aria-label="Default select example" @change="navigate($event, user)"
+    <!-- <select class="form-select form-select-sm" aria-label="Default select example" @change="navigateOld($event, user)"
         v-model="selected_initiative_id">
         <option value="">{{ $t('header.initiative_list.placeholder') }}</option>
         <option v-if="initiatives.length > 0" v-for="initiative in initiatives" :key="initiative.id"
             :value="initiative.id">{{ initiative.client.name }} - {{ initiative.name }}</option>
-    </select>
+    </select> -->
+
+    <multiselect v-model="initiative" :options="initiatives" :multiple="false" label="client_initiative_name"
+        :placeholder="$t('header.initiative_list.placeholder')" track-by="id" @select="navigate($event, user)"
+        select-label="" deselect-label="">
+        <template #option="{ option }">
+            <div class="custom-option">
+                {{ option.client_initiative_name }}
+            </div>
+        </template>
+    </multiselect>
 </template>
 
 <script>
@@ -14,12 +24,17 @@ import { mapActions, mapGetters } from "vuex";
 import store from "../../store/index.js";
 import OpportunityService from '../../services/OpportunityService.js';
 import globalMixin from '@/globalMixin';
+import Multiselect from "vue-multiselect";
 
 export default {
     name: 'HeaderInitiativeDropBoxComponent',
     mixins: [globalMixin],
+    components: {
+        Multiselect
+    },
     data() {
         return {
+            initiative: "",
             initiatives: [],
             selected_initiative_id: this.$route.params.initiative_id ?? this.$route.params.id,
         }
@@ -35,6 +50,8 @@ export default {
             this.selected_initiative_id = this.$route.params.id ?? this.$route.params.initiative_id;
             if (this.selected_initiative_id === undefined) {
                 this.selected_initiative_id = "";
+            } else {
+                this.initiative = this.initiatives.find(initiative => initiative.id == this.selected_initiative_id);
             }
             eventBus.$emit('sidebarSelectHeaderInitiativeId', this.selected_initiative_id);
         },
@@ -51,7 +68,7 @@ export default {
             this.selected_initiative_id = initiativeId;
             eventBus.$emit('sidebarSelectHeaderInitiativeId', this.selected_initiative_id);
         },
-        async navigate(event, userData) {
+        async navigateOld(event, userData) {
             const initiativeId = event.target.value;
             const currentInitiative = this.initiatives.find(initiative => initiative.id == initiativeId);
             store.commit("setCurrentInitiative", currentInitiative);
@@ -73,7 +90,33 @@ export default {
             } else {
                 this.$router.push({ name: 'opportunities' });
             }
-        }
+        },
+        async navigate(event, userData) {
+            const initiativeId = event.id;
+            const currentInitiative = this.initiatives.find(initiative => initiative.id == initiativeId);
+            store.commit("setCurrentInitiative", currentInitiative);
+            eventBus.$emit('sidebarSelectHeaderInitiativeId', initiativeId);
+
+            const routesWithInitiativeId = this.appVariables.ROUTES_NAME_WITH_INITIATIVE_ID;
+            if (routesWithInitiativeId.includes(this.$route.name)) {
+                if (this.$route.name == 'task.detail' && userData.is_admin) {
+                    this.$router.push({ name: 'tasks', params: { id: initiativeId } });
+                } else if (this.$route.name == 'my-tickets' && !userData.is_admin) {
+                    this.$router.push({ name: 'my-tickets', params: { id: initiativeId } });
+                } else {
+                    this.$router.push({ name: this.$route.name, params: { id: initiativeId } });
+                }
+            } else if (initiativeId && userData.is_admin) {
+                this.$router.push({ name: 'solution-design', params: { id: initiativeId } });
+            } else if (initiativeId && !userData.is_admin) {
+                this.$router.push({ name: 'solution-design.detail', params: { id: initiativeId } });
+            } else {
+                this.$router.push({ name: 'opportunities' });
+            }
+        },
+        // nameWithClient({ name, client }) {
+        //     return `${client?.name} - ${name}`
+        // }        
     },
     mounted() {
         this.getInitiativeWithClientData();
@@ -84,3 +127,11 @@ export default {
     },
 }
 </script>
+<style>
+.custom-option {
+    white-space: normal;
+    word-break: break-word;
+    padding: 2px;
+    min-height: 15px;
+}
+</style>
